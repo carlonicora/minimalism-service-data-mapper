6169099db1c156f317e006870aa106e9e742cc2b
carlo <carlo.nicora@gmail.com>
2022-01-28 12:35:50 +0100

    DataObject moved to minimalism-interface-data

--- src/Abstracts/AbstractLoader.php
+++ src/Abstracts/AbstractLoader.php
@@ -3,16 +3,13 @@ namespace CarloNicora\Minimalism\Services\DataMapper\Abstracts;
 
 use CarloNicora\Minimalism\Factories\ObjectFactory;
 use CarloNicora\Minimalism\Interfaces\Cache\Interfaces\CacheInterface;
+use CarloNicora\Minimalism\Interfaces\Data\Abstracts\AbstractIO;
 use CarloNicora\Minimalism\Interfaces\Data\Interfaces\DataInterface;
 use CarloNicora\Minimalism\Interfaces\ServiceInterface;
-use CarloNicora\Minimalism\Interfaces\SimpleObjectInterface;
 use CarloNicora\Minimalism\Services\DataMapper\DataMapper;
-use CarloNicora\Minimalism\Services\DataMapper\Exceptions\RecordNotFoundException;
 use CarloNicora\Minimalism\Services\DataMapper\Interfaces\BuilderInterface;
-use CarloNicora\Minimalism\Services\DataMapper\Interfaces\DataObjectInterface;
-use Exception;
 
-abstract class AbstractLoader implements SimpleObjectInterface
+abstract class AbstractLoader extends AbstractIO
 {
     /** @var BuilderInterface|null  */
     protected ?BuilderInterface $builder=null;
@@ -33,80 +30,10 @@ abstract class AbstractLoader implements SimpleObjectInterface
         protected ?CacheInterface $cache,
     )
     {
+        parent::__construct($this->objectFactory, $this->data);
+
         $this->builder = $mapper->getBuilder();
 
         $this->defaultService = $mapper->getDefaultService();
-    }
-
-    /**
-     * @param array $recordset
-     * @param string|null $recordType
-     * @return array
-     * @throws RecordNotFoundException
-     */
-    protected function returnSingleValue(
-        array $recordset,
-        ?string $recordType=null,
-    ): array
-    {
-        if ($recordset === [] || $recordset === [[]]){
-            throw new RecordNotFoundException(
-                $recordType === null
-                    ? 'Record Not found'
-                    : $recordType . ' not found'
-            );
-        }
-
-        return array_is_list($recordset) ? $recordset[0] : $recordset;
-    }
-
-    /**
-     * @param array $recordset
-     * @param string $objectType
-     * @return DataObjectInterface
-     * @throws Exception
-     */
-    protected function returnSingleObject(
-        array $recordset,
-        string $objectType,
-    ): DataObjectInterface
-    {
-        if ($recordset === [] || $recordset === [[]]){
-            throw new RecordNotFoundException('Record Not found');
-        }
-
-        return new $objectType(
-            objectFactory: $this->objectFactory,
-            data: array_is_list($recordset) ? $recordset[0] : $recordset,
-        );
-    }
-
-    /**
-     * @param array $recordset
-     * @param string $objectType
-     * @return DataObjectInterface[]
-     */
-    protected function returnObjectArray(
-        array $recordset,
-        string $objectType,
-    ): array
-    {
-        $response = [];
-
-        if (array_is_list($recordset)) {
-            foreach ($recordset ?? [] as $record) {
-                $response[] = new $objectType(
-                    objectFactory: $this->objectFactory,
-                    data: $record,
-                );
-            }
-        } else {
-            $response[] = new $objectType(
-                objectFactory: $this->objectFactory,
-                data: $recordset,
-            );
-        }
-
-        return $response;
     }
 }
